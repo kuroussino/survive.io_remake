409cdf07d1bffefe9b746998e21e8ca1780b2e6f
Daniele Soranzio <danielesoranzio3@gmail.com>
2024-06-10 00:28:52 +0200

    Added UI Updates on Events, Events are created but must be invoked

---
+++ Assets/Scripts/Manager/EventsManagerSoranzio.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public static partial class EventsManager 
+{
+    #region Lobby
+    //Event called when trying to join throught the list of lobby in UI
+    public static Action<Lobby> OnClientJoinLobbyWithID;
+    //Event called when creating a lobby throught the UI with a name not equal to "" 
+    public static Action<string> OnHostCreateLobbyWithName;
+    //Event called when the lobby hub must be showed
+    public static Action<Lobby, bool> OnShowLobby;
+    //Event called when the host press the start button
+    public static Action OnHostStartGame;
+    //Event called when a player leave a lobby 
+    public static Action<Unity.Services.Lobbies.Models.Player> OnQuittedLobby;
+    //Event called when the client has entered the lobby
+    public static Action<Unity.Services.Lobbies.Models.Player> OnJoinedLobby;
+    //Event called when the player clicked the leave lobby button, the lobby should update by removing the player
+    public static Action OnLeaveLobbyButton;
+    //Event called after the input of the new player name, lobby should register this data for the player
+    public static Action<string> OnPlayerNameSet;
+    //Event fire when lobby is starting from relay
+    public static Action GameStarting;
+    //
+    public static Action LobbyReady;
+    #endregion
+
+    #region GameUI
+    //Event called when the player is dead.
+    public static Action OnPlayerDead;
+    //Initialize the UI of everyPlayer at the start of the game .
+    public static Action<float,int,A_Weapon> PlayerUIInitialize;
+    //Event called when the local player takes damage.
+    public static Action<float> OnPlayerDamage;
+    //Event called when the player picks up a new weapon, as parameters are needed the weapon and the starting load of ammo on it.
+    public static Action<A_Weapon,int> OnNewWeapon;
+    //Event called when the player heals himself, as the parameter is needed the amount of heal he restore.
+    public static Action<float> OnPlayerHeal;
+    //Event called when the player picks up an health pack.
+    public static Action OnGrabHealthPack;
+    //Event called when the player is using an health pack.Did separate methods in case there are more ways to heal the player.
+    public static Action OnPlayerUseHealthPack;
+    //Event called when the player shoots or reload, as parameter is needed the amount of bullet in the magazine.
+    public static Action<int> OnUpdateBulltes;
+    //Event need to be called when trying to notificate the player(e.g. The Death Zone is closing in 20 seconds)
+    public static Action<object> OnNotificatePlayer;
+    //Event is called by the server, in order to execute for every client, when a player dies and the players alive needs to be updated. As parameter use the player left in the game.
+    public static Action<int> OnUpdatePlayerCount;
+    //Can add killfeed? 
+    #endregion
+}
--- Assets/Scripts/UI/PlayerGameUIManager.cs
+++ Assets/Scripts/UI/PlayerGameUIManager.cs
@@ -1,18 +1,190 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerGameUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Player UI var
+    [Space(5)]
+    [Header("Player UI")]
+    [SerializeField] GameObject playerUI;
+    [SerializeField] TextMeshProUGUI scopeTextUI;
+    [SerializeField] Slider playerHPSlider;
+    [SerializeField] Image PrimaryWeaponSprite;
+    [SerializeField] TextMeshProUGUI PrimaryWeaponName;
+    [SerializeField] TextMeshProUGUI healthPackNumber;
+    [SerializeField] TextMeshProUGUI currentBulletsText;
+    [SerializeField] TextMeshProUGUI playerCountText;
+
+    [Space(10)]
+    [Header("Notification UI")]
+    [SerializeField] TextMeshProUGUI notificationText;
+    [Tooltip("The color of the notification text appearing on screen")]
+    [SerializeField] Color notificationColor;
+
+    #endregion
+    #region Variables
+    private float maxHP;
+    private float currentHP;
+    private int currentBullets;
+    private int currentHealthPacks;
+    private A_Weapon currentWeapon;
+    #endregion
+
+    #region Mono
+    private void OnEnable()
     {
-        
+        EventsManager.PlayerUIInitialize += InitPlayerUI;
+        EventsManager.OnPlayerDead += OnPlayerDeath;
+        EventsManager.OnPlayerDamage += OnDamageTaken;
+        EventsManager.OnPlayerHeal += OnPlayerHeal;
+        EventsManager.OnNewWeapon += OnGetWeapon;
+        EventsManager.OnGrabHealthPack += OnGetHealthPack;
+        EventsManager.OnUpdateBulltes += OnWeaponUpdateBullets;
+        EventsManager.OnNotificatePlayer += OnNotificationText;
+        EventsManager.OnPlayerUseHealthPack += OnUseHealthPack;
+        EventsManager.OnUpdatePlayerCount += UpdatePlayerCount;
+    }
+    private void OnDisable()
+    {
+        EventsManager.PlayerUIInitialize -= InitPlayerUI;
+        EventsManager.OnPlayerDead -= OnPlayerDeath;
+        EventsManager.OnPlayerDamage -= OnDamageTaken;
+        EventsManager.OnPlayerHeal -= OnPlayerHeal;
+        EventsManager.OnNewWeapon -= OnGetWeapon;
+        EventsManager.OnGrabHealthPack -= OnGetHealthPack;
+        EventsManager.OnUpdateBulltes -= OnWeaponUpdateBullets;
+        EventsManager.OnNotificatePlayer -= OnNotificationText;
+        EventsManager.OnPlayerUseHealthPack -= OnUseHealthPack;
+        EventsManager.OnUpdatePlayerCount -= UpdatePlayerCount;
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Custom Methods
+    /// <summary>
+    /// This method is called by an event as soon as the game starts to compile the player UI.
+    /// </summary>
+    private void InitPlayerUI(float maxHP,int scope,A_Weapon weapon)
     {
-        
+        playerUI.SetActive(true);
+        playerHPSlider.maxValue = maxHP;
+        playerHPSlider.minValue= 0;
+        currentHealthPacks = 0;
+        currentWeapon = weapon;
     }
+    /// <summary>
+    /// This method is called by an event when the local player take damage
+    /// </summary>
+    /// <param name="amount"></param>
+    private void OnDamageTaken(float amount)
+    {
+        currentHP -= amount;
+        currentHP = Mathf.Clamp(currentHP, 0, maxHP);
+        playerHPSlider.value = currentHP;
+        CheckLowHealthPlayer(currentHP);
+    }
+    /// <summary>
+    /// This method is called by an event when the local player will heal, updating the amount of heal
+    /// </summary>
+    /// <param name="amount"></param>
+    private void OnPlayerHeal(float amount)
+    {
+        currentHP += amount;
+        currentHP = Mathf.Clamp(currentHP, 0, maxHP);
+        playerHPSlider.value = currentHP;
+        CheckLowHealthPlayer(currentHP);
+    }
+    /// <summary>
+    /// This method is called by an event when the local player get a new primary weapon
+    /// </summary>
+    /// <param name="weapon"></param>
+    private void OnGetWeapon(A_Weapon weapon,int bulletsCurrentlyInWeapon)
+    {
+        currentWeapon = weapon;
+        OnWeaponUpdateBullets(bulletsCurrentlyInWeapon);
+        UpdatePrimaryWeaponUI();
+    }
+    /// <summary>
+    /// This method is going to update the weapon UI only. 
+    /// </summary>
+    /// <param name="weapon"></param>
+    private void UpdatePrimaryWeaponUI()
+    {
+        PrimaryWeaponName.text = currentWeapon.name;
+        PrimaryWeaponSprite.sprite = currentWeapon.GetSpriteItem();
+    }
+    /// <summary>
+    /// This method is called when shooting/reloading or grabbing a new gun with a new number of bullets
+    /// </summary>
+    private void OnWeaponUpdateBullets(int amount)
+    {
+        currentBullets = amount;
+        currentBulletsText.text = currentBullets.ToString();
+    }
+    /// <summary>
+    /// This method is called by an event when the player grab a new HealtPack
+    /// </summary>
+    private void OnGetHealthPack()
+    {
+        UpdateHealthPackNumber(+1);
+    }
+    /// <summary>
+    /// This method is called by an event when the local player use an healthPack;
+    /// </summary>
+    private void OnUseHealthPack()
+    {
+        UpdateHealthPackNumber(-1);
+    }
+    /// <summary>
+    /// This method is called when using or grabbing a new healthpack;
+    /// </summary>
+    /// <param name="amount"></param>
+    private void UpdateHealthPackNumber(int amount)
+    {
+        currentHealthPacks+=amount;  
+        healthPackNumber.text=currentHealthPacks.ToString();
+    }
+    /// <summary>
+    /// This method is called by an event from the server in order to execute on every client when a player is out of the game 
+    /// </summary>
+    private void UpdatePlayerCount(int playerLeftInGame)
+    {
+        playerCountText.text= playerLeftInGame.ToString();
+    }
+    /// <summary>
+    /// This method is called when notificatying the player, like DeathZone or the winner of the game.
+    /// </summary>
+    /// <param name="message"></param>
+    private void OnNotificationText(object message)
+    {
+        notificationText.text = message.ToString();
+        notificationText.color = notificationColor;
+    }
+    /// <summary>
+    /// The moethod is called when taking or healing damage in order to check if the player is lowHealth or not.
+    /// </summary>
+    /// <param name="hp"></param>
+    private void CheckLowHealthPlayer(float hp)
+    {
+        if (hp > maxHP*0.25)
+        {
+            playerHPSlider.fillRect.GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            playerHPSlider.fillRect.GetComponent<Image>().color = Color.red;
+        }
+    }
+    /// <summary>
+    /// This method is called by an event on local player death
+    /// </summary>
+    private void OnPlayerDeath()
+    {
+        playerUI.SetActive(false);
+    }
+    
+    #endregion
 }
